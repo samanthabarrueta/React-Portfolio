a6d41e2ced0b3400a6a942b273c1502989f38654
Samantha <samanthabarrueta@gmail.com>
2020-09-07 23:34:19 -0500

    Updates

---
+++ precache-manifest.8a1a85cdf5eb5a8ba14e9253dde41b42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defabc2fd172e9220d47d9eae3797743",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "cb0ed955e6cf6804ac35",
+    "url": "/React-Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cb0ed955e6cf6804ac35",
+    "url": "/React-Portfolio/static/js/2.1d5762d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.1d5762d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27af1d4389723e79204f",
+    "url": "/React-Portfolio/static/js/main.8fc54178.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "31ff9161ec1136340ea7731851617929",
+    "url": "/React-Portfolio/static/media/IMG_2838edit.31ff9161.jpg"
+  }
+]);
