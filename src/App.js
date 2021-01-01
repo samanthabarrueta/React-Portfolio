0b8b618c7ed619da4aedf0e1873e61269c35ff94
Samantha <samanthabarrueta@gmail.com>
2021-01-01 10:35:06 -0600

    resolve navtabs erros
    convert to navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import Projects from "./components/pages/Projects";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <NavTabs />
+        <Navbar />
         <Route exact path="/React-Portfolio" component={Home} />
         <Route path="/Projects" component={Projects} />
       </div>
