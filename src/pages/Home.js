cb66d759844d5ea7dda8bd92ee79de7902fdbc51
Samantha <samanthabarrueta@gmail.com>
2021-01-01 10:37:47 -0600

    add Card component to home/project pgs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,45 @@
 import React from "react";
+import Card from "../components/Card";
 import Container from "react-bootstrap/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from "../../IMG_2838edit.jpg";
+import Image from "../../src/images/IMG_2838edit.jpg";
 
 const Home = () => {
-  return (
-    <Container>
-      <Row>
-        <Col lg={4}>      
-        <img src={Image} alt="Samantha" width="300" />          
-        </Col>
-        <Col lg={6}> 
-          <h2>Samantha Barrueta</h2> 
-          <p>
-            Full Stack Developer located in Chicago, IL. I have a strong desire for knowledge and I enjoy environments that foster learning. I'm currently enrolled in Northwestern University's Full Stack Coding Bootcamp which is providing me with experience in JavaScript, Node.js, React, HTML, CSS, and MongoDB. View my projects for examples of my ability and skills. As a photography enthusiast, I appreciate the value in creativity, especially when applied with proper technique. This appreciation pushes me to be constantly learning and applying what I've learned to improve my code. 
-          </p>    
-        </Col>
-      </Row>
+    return (
+        <Container>
+            <Card>
+                <Row>
+                    <Col lg={4}>      
+                        <img src={Image} alt="Samantha" width="300" />          
+                    </Col>
+                    <Col lg={6}> 
+                        <h2>Samantha Barrueta</h2> 
+                        <p>
+                        Full Stack Developer located in Chicago, IL. I have a strong desire for knowledge and I enjoy environments that foster learning. I'm currently enrolled in Northwestern University's Full Stack Coding Bootcamp which is providing me with experience in JavaScript, Node.js, React, HTML, CSS, and MongoDB. View my projects for examples of my ability and skills. As a photography enthusiast, I appreciate the value in creativity, especially when applied with proper technique. This appreciation pushes me to be constantly learning and applying what I've learned to improve my code. 
+                        </p>    
+                    </Col>
+                </Row>
 
-      <Row className="row justify-content-md-center">
-        <h3>Contact Me</h3> 
-      </Row>
-      <Row>
-        <Col lg={6}>
-          <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faEnvelope } size="2x" /></Row>
-          <Row className="row justify-content-md-center">samanthabarrueta@gmail.com</Row>
-        </Col>
-        <Col lg={6}>
-          <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faPhone } size="2x" /></Row>
-          <Row className="row justify-content-md-center">(708) 566- 7183</Row>
-        </Col> 
-      </Row>
-    </Container>
-  );
+                <Row className="row justify-content-md-center">
+                    <h3>Contact Me</h3> 
+                </Row>
+
+                <Row>
+                    <Col lg={6}>
+                        <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faEnvelope } size="2x" /></Row>
+                        <Row className="row justify-content-md-center">samanthabarrueta@gmail.com</Row>
+                    </Col>
+                    <Col lg={6}>
+                        <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faPhone } size="2x" /></Row>
+                        <Row className="row justify-content-md-center">(708) 566- 7183</Row>
+                    </Col> 
+                </Row>
+            </Card>
+        </Container>
+    );
 }
 
 export default Home;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,51 @@
 import React from "react";
+import Card from "../components/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 const Projects = () => {
-  return (
-    <div>
-      <h1>Projects</h1>
-      Coming Soon!
-    </div>
-  );
-}
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Projects</h1>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col lg={5}>
+                    <Card>
+                        <p>This is a description of project 1</p>
+                    </Card>
+                </Col>
+
+                <Col lg={5}>
+                    <Card>
+                        <p>This is a description of project 2</p>
+                    </Card>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col lg={5}>
+                    <Card>
+                        <p>This is a description of project 3</p>
+                    </Card>
+                </Col>
+
+                <Col lg={5}>
+                    <Card>
+                        <p>This is a description of project 4</p>
+                    </Card>
+                </Col>
+            </Row>
+
+            
+            
+            
+        </Container>
+    );
+};
 
 export default Projects;
