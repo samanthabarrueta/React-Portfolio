698d9f5585afdfb06b9356e707cb3afea86602ce
Samantha <samanthabarrueta@gmail.com>
2021-01-06 21:01:35 -0600

    update navbar layout

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,26 +10,31 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <Nav defaultActiveKey="/home" as="ul">
-                <Nav.Item as="li">
-                    <Link to="/React-Portfolio" className={location.pathname === "/React-Portfolio" ? "nav-link active" : "nav-link"}>
-                        Home
-                    </Link>
-                </Nav.Item>
-                <Nav.Item as="li">
+            <Nav defaultActiveKey="/home">
+                <Nav.Item className="spacing">
                     <li className="nav-item">
                         <a href="https://www.linkedin.com/in/samantha-barrueta/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={ faLinkedinIn } /></a>
                     </li>
                 </Nav.Item>
-                <Nav.Item as="li">
-                    <Link to="/Projects" className={location.pathname === "/Projects" ? "nav-link active" : "nav-link"}>
-                        Projects
-                    </Link>
-                </Nav.Item>
-                <Nav.Item as="li">
+
+                <Nav.Item className="spacing">
                     <li className="nav-item">
                         <a href="https://github.com/samanthabarrueta" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={ faGithub } /></a>
                     </li>
+                </Nav.Item>
+            </Nav>
+
+            <Nav>
+                <Nav.Item>
+                    <Link to="/React-Portfolio" className={location.pathname === "/React-Portfolio" ? "nav-link active" : "nav-link"}>
+                        About
+                    </Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                    <Link to="/Projects" className={location.pathname === "/Projects" ? "nav-link active" : "nav-link"}>
+                        Projects
+                    </Link>
                 </Nav.Item>
             </Nav>
         </div>
