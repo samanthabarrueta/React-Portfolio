37562eef4eb3e964eafa9c8f3243aaacd3ce699a
Samantha <samanthabarrueta@gmail.com>
2021-01-09 23:47:39 -0600

    move projects to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,6 +40,60 @@ const Home = () => {
                     </Row>
                 </Card>
             </Row> 
+
+            <Row>
+                <Col>
+                    <h1>Projects</h1>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
+
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
+
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
+            </Row>  
         </Container>
     );
 };
