40c3dcdeb45b196a6e6eded08880ebad4552612c
Samantha <samanthabarrueta@gmail.com>
2021-01-10 21:57:55 -0600

    remove projects page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
 
 const App = () => {
   return (
     <Router>
         <Navbar />
-        <Route exact path="/React-Portfolio" component={Home} />
-        <Route path="/Projects" component={Projects} />      
+        <Route exact path="/" component={Home} />
     </Router>
   );
   
