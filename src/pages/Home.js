746f7437af7b32192a933d99428a1f7bb000e312
Samantha <samanthabarrueta@gmail.com>
2021-01-13 21:04:33 -0600

    remove card component as main container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,93 +10,90 @@ import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
     return (
         <Container fluid="lg">
+            <Row className="text-center">
+                <Col lg={12}>      
+                    <img src={Image} alt="Samantha" width="300" id="about"/>          
+                </Col>
+            </Row>
+
+            <Row className="text-center">
+                <Col lg={12}> 
+                    <h1>Hi! I'm Samantha</h1> 
+                    <h2>Full Stack Web Developer</h2>
+                    <p>
+                    Welcome, I'm Samantha Barrueta. I have a strong desire for knowledge and I enjoy environments that foster learning. I've obtained a certificate in Full Stack Web Development, from Northwestern University's Coding Bootcamp. I have experience in JavaScript, Node.js, React, HTML, CSS, and MongoDB. View my projects for examples of my ability and skills. As a photography enthusiast, I appreciate the value in creativity, especially when applied with proper technique. This appreciation pushes me to be constantly learning and applying what I've learned to improve my code. 
+                    </p>    
+                </Col>
+            </Row>
+
             <Row>
-                <Card >
-                    <Row className="text-center">
-                        <Col lg={12}>      
-                            <img src={Image} alt="Samantha" width="300" id="about"/>          
-                        </Col>
-                    </Row>
+                <Col>
+                    <h1 id="projects">Projects</h1>
+                </Col>
+            </Row>
 
-                    <Row className="text-center">
-                        <Col lg={12}> 
-                            <h2>Samantha Barrueta</h2> 
-                            <p>
-                            Full Stack Developer located in Chicago, IL. I have a strong desire for knowledge and I enjoy environments that foster learning. I've obtained a certificate in Full Stack Web Development, from Northwestern University's Coding Bootcamp. I have experience in JavaScript, Node.js, React, HTML, CSS, and MongoDB. View my projects for examples of my ability and skills. As a photography enthusiast, I appreciate the value in creativity, especially when applied with proper technique. This appreciation pushes me to be constantly learning and applying what I've learned to improve my code. 
-                            </p>    
-                        </Col>
-                    </Row>
+            <Row>
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
 
-                    <Row>
-                        <Col>
-                            <h1 id="projects">Projects</h1>
-                        </Col>
-                    </Row>
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
+            </Row>
 
-                    <Row>
-                        <Col lg={6}>
-                            <Card>
-                                <h1>Project Name</h1>
-                                <p>Quick Description</p>
-                                <p>Link to deployed App</p>
-                                <p>Login Info if Applicable</p>
-                                <p>Link to Github</p>
-                                <p>Images</p>
-                            </Card>
-                        </Col>
+            <Row>
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
 
-                        <Col lg={6}>
-                            <Card>
-                                <h1>Project Name</h1>
-                                <p>Quick Description</p>
-                                <p>Link to deployed App</p>
-                                <p>Login Info if Applicable</p>
-                                <p>Link to Github</p>
-                                <p>Images</p>
-                            </Card>
-                        </Col>
-                    </Row>
+                <Col lg={6}>
+                    <Card>
+                        <h1>Project Name</h1>
+                        <p>Quick Description</p>
+                        <p>Link to deployed App</p>
+                        <p>Login Info if Applicable</p>
+                        <p>Link to Github</p>
+                        <p>Images</p>
+                    </Card>
+                </Col>
+            </Row>
 
-                    <Row>
-                        <Col lg={6}>
-                            <Card>
-                                <h1>Project Name</h1>
-                                <p>Quick Description</p>
-                                <p>Link to deployed App</p>
-                                <p>Login Info if Applicable</p>
-                                <p>Link to Github</p>
-                                <p>Images</p>
-                            </Card>
-                        </Col>
+            <Row className="row justify-content-md-center">
+                <h3 id="contact">Contact Me</h3> 
+            </Row>
 
-                        <Col lg={6}>
-                            <Card>
-                                <h1>Project Name</h1>
-                                <p>Quick Description</p>
-                                <p>Link to deployed App</p>
-                                <p>Login Info if Applicable</p>
-                                <p>Link to Github</p>
-                                <p>Images</p>
-                            </Card>
-                        </Col>
-                    </Row>
-
-                    <Row className="row justify-content-md-center">
-                        <h3 id="contact">Contact Me</h3> 
-                    </Row>
-
-                    <Row>
-                        <Col lg={6}>
-                            <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faEnvelope } size="2x" /></Row>
-                            <Row className="row justify-content-md-center">samanthabarrueta@gmail.com</Row>
-                        </Col>
-                        <Col lg={6}>
-                            <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faPhone } size="2x" /></Row>
-                            <Row className="row justify-content-md-center">(708) 566- 7183</Row>
-                        </Col> 
-                    </Row>
-                </Card>
-            </Row>  
+            <Row>
+                <Col lg={6}>
+                    <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faEnvelope } size="2x" /></Row>
+                    <Row className="row justify-content-md-center">samanthabarrueta@gmail.com</Row>
+                </Col>
+                <Col lg={6}>
+                    <Row className="row justify-content-md-center"><FontAwesomeIcon icon={ faPhone } size="2x" /></Row>
+                    <Row className="row justify-content-md-center">(708) 566- 7183</Row>
+                </Col> 
+            </Row>
         </Container>
     );
 };
